4b600d9f165ca204c72d8217a91caae7bb249c16
Vbobell <vinicius_bobel@hotmail.com>
2017-06-27 20:52:37 -0300

    removendo arquivos

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var request = require('request');
+var express = require('express');
+var app = express();
 
-var server = http.createServer(function (requisitado, resposta) {
+app.set('port', (process.env.PORT || 5000));
 
-    this.resultado = url.parse(requisitado.url, true);
+app.use(express.static(__dirname + '/public'));
 
-    if (this.resultado.pathname == "/favicon.ico") { return }
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-    resposta.writeHead(200, {'Content-Type': 'text/plain'});
-    resposta.end('Hello Node.JS!');
-}).listen(5000);
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
